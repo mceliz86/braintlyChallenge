ae026cd8d2b38b3edc158bfc7a083da163a939bd
Matias Celiz <matias_goc@hotmail.com>
2022-11-02 23:45:12 -0300

    Create basic task component template

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,13 +1,14 @@
 <template>
-
+<task-component/>
 </template>
 
 <script>
+import taskComponent from './components/task-component.vue';
 
 export default {
   name: 'App',
   components: {
-
+    taskComponent,
   }
 }
 </script>
---
+++ todo-list/src/components/task-component.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1 class="display-4 text-center">Tasks</h1>
+        <hr>
+        <div class="col-lg-8 offset-lg-2">
+            <div class="card mt-4">
+                <div class="card-body">
+                    <div class="input-group">
+                        <input type="text" class="form-control form-control-lg" placeholder="Add new task">
+                        <div class="input-group-append">
+                            <button class="btn btn-success btn-lg">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'task-component'
+}
+</script>
+
+<style scoped>
+    
+</style>
